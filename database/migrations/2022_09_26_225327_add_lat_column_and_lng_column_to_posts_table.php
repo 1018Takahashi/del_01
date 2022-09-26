8830a5de309c24dc6d7800f6ee7044acfbf167d2
1018Takahashi <kazuharurivai@gmail.com>
2022-09-26 23:45:20 +0900

    Table Repair

---
+++ database/migrations/2022_09_26_225327_add_lat_column_and_lng_column_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLatColumnAndLngColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('lat', 50)->nullable();
+            $table->string('lng', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+}
